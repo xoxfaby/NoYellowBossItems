9f91400f210e8c67fb0a0267aab32dda84ba96af
XoXFaby <xoxfaby@gmail.com>
2021-04-07 00:26:34 +0200

    Initial Commit

---
+++ NoYellowBossItems.fs
@@ -0,0 +1,38 @@
+﻿namespace NoYellowBossItems
+open BepInEx
+open RoR2
+open System.Security
+open System.Security.Permissions
+
+[<assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)>]
+[<UnverifiableCode>]
+do ()
+
+[<BepInPlugin("com.xoxfaby.NoYellowBossDrops", "NoYellowBossDrops", "1.0.0.0")>]
+type NoYellowBossItems() = 
+    inherit BaseUnityPlugin()
+    
+    let mutable ItemConfig = Seq.empty 
+
+    member this.hook_BossGroup_DropRewards = On.RoR2.BossGroup.hook_DropRewards (fun (orig:  On.RoR2.BossGroup.orig_DropRewards) (self : RoR2.BossGroup) ->
+        self.bossDrops <- System.Collections.Generic.List<PickupIndex> (seq {
+            for pickupIndex in self.bossDrops do
+                if not (Seq.contains (ItemCatalog.GetItemDef pickupIndex.pickupDef.itemIndex).nameToken ItemConfig) then
+                    pickupIndex
+        })
+        orig.Invoke self)
+
+
+    member this.Awake () =
+        On.RoR2.BossGroup.add_DropRewards this.hook_BossGroup_DropRewards
+
+    member this.Start () =
+        ItemConfig <- seq {
+            for itemDef in ItemCatalog.itemDefs do 
+                if itemDef.tier = ItemTier.Lunar then 
+                    let configEntry = this.Config.Bind("Disabled Boss Items", itemDef.nameToken, true, "Set true to stop this item from dropping.")
+                    if configEntry.Value then
+                        configEntry.Definition.Key
+        }
+
+        
---
+++ Thunderstore/README.md
@@ -0,0 +1,23 @@
+# NoYellowBossItems
+
+Does what it says on the box. 
+Teleporter events will no longer drop yellow items. You can disable each in the config. By default they are all disabled.
+
+## Support Me
+
+If you like what I'm doing, consider supporting me through GitHub Sponsors so I can keep doing it:
+
+https://github.com/sponsors/xoxfaby
+
+## Help & Feedback
+
+If you need help or have suggestions, create an issue on github, join my discord or find me on the RoR2 Modding Discord 
+
+[My Discord](https://discord.gg/Zy2HSB4) XoXFaby#1337
+
+https://github.com/xoxfaby/BetterAPI
+
+## Changelog
+
+## v1.0.0
+ - Inital Release
